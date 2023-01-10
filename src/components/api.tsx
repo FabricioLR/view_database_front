c082d8bd041eb9135622861a4e72087d5a31610e
FabricioLR <dimamitoyt@gmail.com>
2023-01-09 21:19:20 -0300

    update api url

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,9 +1,10 @@
 import axios from "axios"
 
 //"http://localhost:4000/"
+//"https://view-database-back.onrender.com"
 
 const api = axios.create({
-   baseURL: "http://localhost:4000/"
+   baseURL: "https://view-database-back.onrender.com"
 })
 
 export default api
